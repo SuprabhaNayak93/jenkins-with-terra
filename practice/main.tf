43c0efdaf5d9c4550283aa00be0846566fd6dddc
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-29 19:20:25 +0530

    Update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -5,7 +5,7 @@ region = "ap-southeast-2"
 
 resource "aws_instance" "name" {
     ami = "ami-0b519d86134871cb6"
-    instance_type = "t3.micro"
+    instance_type = "t2.medium"
     tags = {
       Name="Server-J"
     }
