56eb04545bfe03f5708b1cfa5b29419997298d50
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-29 20:41:39 +0530

    Update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 
 resource "aws_instance" "name" {
-    ami = "ami-0b519d86134871cb6"
+    ami = "ami-02b64aa047cb5edf5"
     instance_type = "t2.medium"
     tags = {
       Name="Server-J"
